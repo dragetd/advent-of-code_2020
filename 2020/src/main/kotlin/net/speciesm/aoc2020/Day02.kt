01a9d7a1dd2449d3e5665f03370fc710a73732a4
Michael Ko. Gajda <draget@speciesm.net>
2020-12-03 22:36:39 +0100

    Improve Day02

--- 2020/src/main/kotlin/net/speciesm/aoc2020/Day02.kt
+++ 2020/src/main/kotlin/net/speciesm/aoc2020/Day02.kt
@@ -6,10 +6,8 @@ object Day02 {
     private fun isValid(min: Int, max: Int, char: Char, pass: String): Boolean =
         (pass.filter { it == char }.count() in min..max)
 
-    fun solve(inputs: List<String>): Int {
-        return inputs
-            .mapNotNull { policy.find(it)?.destructured }
-            .count { (min, max, char, pass) -> isValid(min.toInt(), max.toInt(), char.first(), pass) }
-    }
+    fun solve(inputs: List<String>): Int = inputs
+        .mapNotNull { policy.find(it)?.destructured }
+        .count { (min, max, char, pass) -> isValid(min.toInt(), max.toInt(), char.first(), pass) }
 }
 
