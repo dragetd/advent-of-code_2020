38ea7b99dd72084c8bb8ef1feb7611e1fc95c267
Michael Ko. Gajda <draget@speciesm.net>
2020-12-03 21:39:11 +0100

    Improve Day3

--- 2020/src/main/kotlin/net/speciesm/aoc2020/Day03.kt
+++ 2020/src/main/kotlin/net/speciesm/aoc2020/Day03.kt
@@ -2,15 +2,14 @@ package net.speciesm.aoc2020
 
 object Day03 {
     fun solve(inputs: List<String>): Int {
-        val xStep = 3
+        return treesOnSlope(inputs, 3, 1)
+    }
+
+    private fun treesOnSlope(inputs: List<String>, rightStep: Int, downStep: Int): Int {
         var acc = 0
-
-        for ((i, line) in inputs.withIndex()) {
-            if (i > 0) {
-                if (line[i * xStep % line.length] == '#') acc++
-            }
+        for (i in 1 until inputs.size step downStep) {
+            if (inputs[i][i * rightStep % inputs[i].length] == '#') acc++
         }
-
         return acc
     }
 }
