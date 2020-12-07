dba6391342a304f33c7fd509c995ac2f66399e9d
Michael Ko. Gajda <draget@speciesm.net>
2020-12-07 21:29:07 +0100

    Refactor Day06

--- 2020/src/main/kotlin/net/speciesm/aoc2020/Day06.kt
+++ 2020/src/main/kotlin/net/speciesm/aoc2020/Day06.kt
@@ -1,11 +1,13 @@
 package net.speciesm.aoc2020
 
 object Day06 {
-    fun solve(inputs: List<String>): Int {
-        // Whole input with spaces instead of newlines, then separated by empty line
-        val inputString = inputs.fold("") { line, next -> "$line $next" }
-        val groups = inputString.split("  ")
-        return groups
+    // Whole input with spaces instead of newlines, then separated by empty line
+    private fun asGroupStrings(inputs: List<String>): List<String> =
+        inputs.fold("") { line, next -> "$line $next" }
+            .split("  ")
+
+    fun solve(inputs: List<String>): Int =
+        asGroupStrings(inputs)
             .map { it.replace(" ", "").toCharArray().distinct() }
             .sumBy { it.size }
     }
