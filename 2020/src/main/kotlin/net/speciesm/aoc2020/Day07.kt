bcfbccf747cb9db1ef4c63d9afce3b0939fcbf0f
Michael Ko. Gajda <draget@speciesm.net>
2020-12-12 17:48:20 +0100

    Refactor Day07_1

--- 2020/src/main/kotlin/net/speciesm/aoc2020/Day07.kt
+++ 2020/src/main/kotlin/net/speciesm/aoc2020/Day07.kt
@@ -9,25 +9,28 @@ object Day07 {
     private val rulePattern = Regex("""^($colors) bags contain (.*).$""")
     private val bagRulesPattern = Regex("""(\d+) ($colors) bags?|$nobags""")
 
-    private fun bagRulesToColors(bagRules: String): Map<String, Int> =
-        if (bagRules == nobags) mapOf() else bagRulesPattern.findAll(bagRules)
+    private fun List<String>.toRules(): Map<String, Map<String, Int>> =
+        this.mapNotNull { rulePattern.matchEntire(it)?.destructured }
+            .map { (subject, bagRules) -> subject to bagRules.toColors() }
+            .toMap()
+
+    private fun String.toColors(): Map<String, Int> =
+        if (this == nobags) mapOf() else bagRulesPattern.findAll(this)
             .mapNotNull { it.destructured }
             .map { (count, allowedColor) -> allowedColor to count.toInt() }
             .toMap()
 
-    private fun canReachShiny(rules: Map<String, Map<String, Int>>, query: String): Boolean =
-        canReachShinyRecursive(rules, query, 0)
+    private fun String.canReachShiny(rules: Map<String, Map<String, Int>>): Boolean =
+        this.canReachShinyRecursive(rules, 0)
 
-    private fun canReachShinyRecursive(rules: Map<String, Map<String, Int>>, query: String, depth: Int): Boolean {
+    private fun String.canReachShinyRecursive(rules: Map<String, Map<String, Int>>, depth: Int): Boolean {
         if (depth > MAX_DEPTH) throw IllegalStateException("Rules to deep! Depth at $depth")
-        if (rules[query]?.keys?.contains(SHINY_BAG) == true) return true
-        return (rules[query]?.any { canReachShinyRecursive(rules, it.key, depth + 1) } == true)
+        if (rules[this]?.keys?.contains(SHINY_BAG) == true) return true
+        return (rules[this]?.any { it.key.canReachShinyRecursive(rules, depth + 1) } == true)
     }
 
     fun solve(inputs: List<String>): Int {
-        val ruleMap = inputs.mapNotNull { rulePattern.matchEntire(it)?.destructured }
-            .map { (subject, ruleParts) -> subject to bagRulesToColors(ruleParts) }
-            .toMap()
-        return ruleMap.keys.count { canReachShiny(ruleMap, it) }
+        val rules = inputs.toRules()
+        return rules.keys.count { it.canReachShiny(rules) }
     }
 }
